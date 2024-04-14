e9586a15b710814bbf7bbbbfb8702eec6117fb87
=shalwin04 <=shalwinsanju.25cs@licet.ac.in>
2024-04-14 11:52:32 +0530

    email completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from "./logo.svg";
-import Vaultrafiki from "./images/Vault-rafiki.png";
-import YoungandHappy from "./images/Young and happy-bro.png";
-import Segmentation from "./images/Segmentation-pana.png";
-import Finance from "./images/Finance-amico.png";
-import Filesearch from "./images/File searching-pana (1).png";
-import Financepana from "./images/Finance-pana (1).png";
-import Invoice from "./images/Invoice-pana (1).png";
-import logo1 from "./images/PNG.png";
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaLinkedinIn } from "react-icons/fa";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Form from "./Form";
-import { useNavigate } from "react-router-dom";
-import Home from "./Home";
 import AllRoutes from "./AllRoutes";
 
 function App() {
- 
-
   return (
     <Router>
-      <AllRoutes/>
+      <AllRoutes />
     </Router>
   );
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,11 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import logo1 from "./images/PNG.png";
+import axios from "axios";
+import emailjs from '@emailjs/browser';
+
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [userType, setUserType] = useState("Individual");
+  const [companyName, setCompanyName] = useState("");
+  const [designation, setDesignation] = useState("");
 
   const handleUserTypeChange = (e) => {
     setUserType(e.target.value);
+  };
+
+  const form = useRef();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   // Create a data object with form values
+  //   const formData = {
+  //     name,
+  //     email,
+  //     phoneNumber,
+  //     userType,
+  //     companyName: userType === "Company" ? companyName : "",
+  //     designation: userType === "Company" ? designation : "",
+  //   };
+
+  //   // Send form data to the backend
+  //   try {
+  //     const response = await axios.post("http://localhost:4000/api/form-submit", {
+  //       // method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+  //     // const response = await axios.post("http://localhost:4000/api/form-submit", formData);
+
+  //     if (response.ok) {
+  //       console.log("Form submitted successfully!");
+  //       alert("Submitted Sucessfully");
+  //       // Optionally, reset form state here
+  //     } else {
+  //       console.error("Failed to submit form");
+  //       alert("failed to submit");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error submitting form:", error);
+  //   }
+  // };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_o8wpxse', 'template_zvcnroz', form.current, {
+        publicKey: 'cLn7W-9LdheIYsOUR',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert("Submitted Sucessfully");
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert("Failed to submit");
+        },
+      );
   };
 
   return (
@@ -26,53 +92,75 @@ const Form = () => {
         </div>
       </header>
       <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
-        <form className="mt-8 max-w-md mx-auto p-6 border border-gray-300 rounded-lg">
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="mt-8 max-w-md mx-auto p-6 border border-gray-300 rounded-lg"
+        >
           <div className="mb-4">
-            <label htmlFor="firstName" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="firstName"
+              className="block text-gray-700 font-bold mb-2"
+            >
               Name*
             </label>
             <input
               type="text"
               id="firstName"
-              name="firstName"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Name"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2"
+            >
               Email Address*
             </label>
             <input
               type="email"
               id="email"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Email Address"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="phNumber" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="phNumber"
+              className="block text-gray-700 font-bold mb-2"
+            >
               Phone Number
             </label>
             <input
               type="text"
               id="phNumber"
               name="phNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
               className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Phone Number"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="userType" className="block text-gray-700 font-bold mb-2">
+            <label
+              htmlFor="userType"
+              className="block text-gray-700 font-bold mb-2"
+            >
               User Type*
             </label>
             <select
               id="userType"
               name="userType"
-              className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               value={userType}
               onChange={handleUserTypeChange}
+              className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             >
               <option value="Individual">Individual</option>
               <option value="Company">Company</option>
@@ -81,25 +169,35 @@ const Form = () => {
           {userType === "Company" && (
             <>
               <div className="mb-4">
-                <label htmlFor="companyName" className="block text-gray-700 font-bold mb-2">
+                <label
+                  htmlFor="companyName"
+                  className="block text-gray-700 font-bold mb-2"
+                >
                   Company Name*
                 </label>
                 <input
                   type="text"
                   id="companyName"
                   name="companyName"
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
                   className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Company Name"
                 />
               </div>
               <div className="mb-4">
-                <label htmlFor="designation" className="block text-gray-700 font-bold mb-2">
+                <label
+                  htmlFor="designation"
+                  className="block text-gray-700 font-bold mb-2"
+                >
                   Designation*
                 </label>
                 <input
                   type="text"
                   id="designation"
                   name="designation"
+                  value={designation}
+                  onChange={(e) => setDesignation(e.target.value)}
                   className="appearance-none border rounded-sm w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Designation"
                 />
