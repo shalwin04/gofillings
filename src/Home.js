d049423c2f9b1b23f50af2911003bd31a282449d
NikithaRaj <98222648+Nikitha-Raj-08@users.noreply.github.com>
2024-04-16 21:03:17 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import AnalyticsAmico from "./images/Analytics-amico.png";
 import Vaultrafiki from "./images/Vault-rafiki.png";
 import YoungandHappy from "./images/Young and happy-bro.png";
@@ -12,223 +13,226 @@ import { FaXTwitter, FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Form from "./Form";
-import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const navigate = useNavigate();
+  const [showForm, setShowForm] = useState(false);
 
-  const handleClick = () => {
-    navigate("/form");
+  const handleButtonClick = () => {
+    setShowForm(true);
   };
-  return (
+
+  const handleCloseForm = () => {
+    setShowForm(false);
+  };
+return (
     <div>
-      <header className="flex items-center justify-center bg-gray-800 text-white p-4">
-        <div className="flex items-center justify-between w-full max-w-screen-lg mx-auto">
-          <div className="flex items-center">
+    <header className="flex items-center justify-center bg-gray-800 text-white p-4">
+      <div className="flex items-center justify-between w-full max-w-screen-lg mx-auto">
+        <div className="flex items-center">
+          <img
+            src={logo1}
+            alt="Logo"
+            className="w-16 h-16 mr-3" // Adjust the width and height here
+          />
+        </div>
+        <div>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={handleButtonClick}>
+            Contact Us
+          </button>
+        </div>
+      </div>
+    </header>
+    <main>
+      <div className="flex items-center justify-center mt-8">
+        <div className="flex items-center w-full max-w-screen-lg mx-auto">
+          <div className="flex items-center justify-between w-full">
+            <div>
+              <h1 className="text-3xl font-bold text-green-500 text-center">
+                Are you working somewhere?
+              </h1>
+              <div className="mt-6">
+                <p className="text-4xl font-bold text-blue-500 text-center">
+                  Have you filled your Income Tax Returns?
+                </p>
+              </div>
+              <div className="flex items-center justify-center mt-4">
+              <button
+                className="mt-6 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                onClick={handleButtonClick}
+              >
+                Click here
+              </button>
+              </div>
+            </div>
             <img
-              src={logo1}
+              src={AnalyticsAmico}
               alt="Logo"
-              className="w-16 h-16 mr-3" // Adjust the width and height here
+              className="w-full md:w-1/2 ml-8"
             />
           </div>
-          <div>
-            <button
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-              onClick={handleClick}
-            >
-              Contact Us
-            </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-16">
+        <div className="flex items-center w-full max-w-screen-lg mx-auto">
+          <div className="flex items-center justify-between w-full">
+            <div>
+              <h1 className="text-3xl text-blue-800 text-center">
+                GoFilings is a Cloud based Accounting platform through which
+                we offer all accounting services like Filing Income Tax
+                Returns, GST Returns etc., at users convenience.
+              </h1>
+              <div className="mt-6">
+                <p className="text-6xl text-center font-bold text-blue-500 ">
+                  Why <span className="text-green-500">Go</span>Filings?
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </header>
-      <main>
-        <div className="flex items-center justify-center mt-8">
-          <div className="flex items-center w-full max-w-screen-lg mx-auto">
-            <div className="flex items-center justify-between w-full">
+      </div>
+      <div className="flex items-center justify-center mt-6">
+        <div className="flex flex-wrap justify-center w-full max-w-screen-lg mx-auto">
+          <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
+            <img src={YoungandHappy} alt="Image 1" className="w-1/2" />
+            <div className="text-center mt-4">
+              <p className="text-lg">
+                We have a young and energetic team, who are commited to
+                their work
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
+            <img src={Vaultrafiki} alt="Image 2" className="w-1/2" />
+            <div className="text-center mt-4">
+              <p className="text-lg">
+                We have you secured, all your details are safe with us.
+                You're privacy and security
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
+            <img src={Segmentation} alt="Image 3" className="w-1/2" />
+            <div className="text-center mt-4">
+              <p className="text-lg">
+                We ensure accurate tax filings, we assure complete and
+                accurate filing.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
+            <img src={Finance} alt="Image 4" className="w-1/2" />
+            <div className="text-center mt-4">
+              <p className="text-lg">
+                We make sure you get the maximum tax refund out of your tax
+                filing.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-6">
+        <div className="flex items-center w-full max-w-screen-lg mx-auto">
+          <div className="flex items-center justify-between w-full">
+            <img
+              src={Filesearch}
+              alt="Logo"
+              className="w-full md:w-1/2 ml-8"
+            />
+            <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
               <div>
-                <h1 className="text-3xl font-bold text-green-500 text-center">
+                <h1 className="text-3xl text-blue-500 font-bold text-center">
                   Are you working somewhere?
                 </h1>
-                <div className="mt-6">
-                  <p className="text-4xl font-bold text-blue-500 text-center">
-                    Have you filled your Income Tax Returns?
+                <div className="mt-4">
+                  <p className="text-center">
+                    Have no time to file your Income tax returns.
+                  </p>
+                  <p className="text-xl text-blue-500 text-center">
+                    Get your returns filed in a few simple steps.
                   </p>
                 </div>
                 <div className="flex items-center justify-center mt-4">
-                  <button
-                    className="mt-6 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-                    onClick={handleClick}
-                  >
-                    Click here
+                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                  onClick={handleButtonClick}>
+                    Start filing
                   </button>
                 </div>
               </div>
-              <img
-                src={AnalyticsAmico}
-                alt="Logo"
-                className="w-full md:w-1/2 ml-8"
-              />
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center mt-16">
-          <div className="flex items-center w-full max-w-screen-lg mx-auto">
-            <div className="flex items-center justify-between w-full">
+      </div>
+      <div className="flex items-center justify-center mt-6">
+        <div className="flex items-center w-full max-w-screen-lg mx-auto">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
               <div>
-                <h1 className="text-3xl text-blue-800 text-center">
-                  GoFilings is a Cloud based Accounting platform through which
-                  we offer all accounting services like Filing Income Tax
-                  Returns, GST Returns etc., at users convenience.
+                <h1 className="text-3xl  font-bold text-blue-500 text-center">
+                  Are you a Business Owner?
                 </h1>
-                <div className="mt-6">
-                  <p className="text-6xl text-center font-bold text-blue-500 ">
-                    Why <span className="text-green-500">Go</span>Filings?
+                <div className="mt-4">
+                  <p className="text-center">
+                    Have no time to file your Income tax returns.
+                  </p>
+                  <p className="text-xl text-blue-500 text-center">
+                    Get your returns filed in a few simple steps.
                   </p>
                 </div>
+                <div className="flex items-center justify-center mt-4">
+                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" 
+                  onClick={handleButtonClick}>
+                    Start filing
+                  </button>
+                </div>
               </div>
             </div>
+            <img
+              src={Financepana}
+              alt="Logo"
+              className="w-full md:w-1/2 ml-8"
+            />
           </div>
         </div>
-        <div className="flex items-center justify-center mt-6">
-          <div className="flex flex-wrap justify-center w-full max-w-screen-lg mx-auto">
-            <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
-              <img src={YoungandHappy} alt="Image 1" className="w-1/2" />
-              <div className="text-center mt-4">
-                <p className="text-lg">
-                  We have a young and energetic team, who are commited to their
-                  work
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
-              <img src={Vaultrafiki} alt="Image 2" className="w-1/2" />
-              <div className="text-center mt-4">
-                <p className="text-lg">
-                  We have you secured, all your details are safe with us. You're
-                  privacy and security
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
-              <img src={Segmentation} alt="Image 3" className="w-1/2" />
-              <div className="text-center mt-4">
-                <p className="text-lg">
-                  We ensure accurate tax filings, we assure complete and
-                  accurate filing.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center w-1/2 md:w-1/4">
-              <img src={Finance} alt="Image 4" className="w-1/2" />
-              <div className="text-center mt-4">
-                <p className="text-lg">
-                  We make sure you get the maximum tax refund out of your tax
-                  filing.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-center mt-6">
-          <div className="flex items-center w-full max-w-screen-lg mx-auto">
-            <div className="flex items-center justify-between w-full">
-              <img
-                src={Filesearch}
-                alt="Logo"
-                className="w-full md:w-1/2 ml-8"
-              />
-              <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
-                <div>
-                  <h1 className="text-3xl text-blue-500 font-bold text-center">
-                    Are you working somewhere?
-                  </h1>
-                  <div className="mt-4">
-                    <p className="text-center">
-                      Have no time to file your Income tax returns.
-                    </p>
-                    <p className="text-xl text-blue-500 text-center">
-                      Get your returns filed in a few simple steps.
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-center mt-4">
-                    <button
-                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-                      onClick={handleClick}
-                    >
-                      Start filing
-                    </button>
-                  </div>
+      </div>
+      <div className="flex items-center justify-center mt-6">
+        <div className="flex items-center w-full max-w-screen-lg mx-auto">
+          <div className="flex items-center justify-between w-full">
+            <img
+              src={Invoice}
+              alt="Logo"
+              className="w-full md:w-1/2 ml-8"
+            />
+            <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
+              <div>
+                <h1 className="text-3xl text-blue-500 font-bold text-center">
+                  Looking for any other Accounting Services?
+                </h1>
+                <div className="mt-4">
+                  <p className="text-center">
+                    Like filing your GST returns, Auditing etc.
+                  </p>
+                  <p className="text-xl text-blue-500 text-center">
+                    Reach Out to Us.
+                  </p>
+                </div>
+                <div className="flex items-center justify-center mt-4">
+                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" 
+                  onClick={handleButtonClick}>
+                    Start filing
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center mt-6">
-          <div className="flex items-center w-full max-w-screen-lg mx-auto">
-            <div className="flex items-center justify-between w-full">
-              <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
-                <div>
-                  <h1 className="text-3xl  font-bold text-blue-500 text-center">
-                    Are you a Business Owner?
-                  </h1>
-                  <div className="mt-4">
-                    <p className="text-center">
-                      Have no time to file your Income tax returns.
-                    </p>
-                    <p className="text-xl text-blue-500 text-center">
-                      Get your returns filed in a few simple steps.
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-center mt-4">
-                    <button
-                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-                      onClick={handleClick}
-                    >
-                      Start filing
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <img
-                src={Financepana}
-                alt="Logo"
-                className="w-full md:w-1/2 ml-8"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-center mt-6">
-          <div className="flex items-center w-full max-w-screen-lg mx-auto">
-            <div className="flex items-center justify-between w-full">
-              <img src={Invoice} alt="Logo" className="w-full md:w-1/2 ml-8" />
-              <div className="flex items-center justify-center w-full max-w-screen-lg mx-auto">
-                <div>
-                  <h1 className="text-3xl text-blue-500 font-bold text-center">
-                    Looking for any other Accounting Services?
-                  </h1>
-                  <div className="mt-4">
-                    <p className="text-center">
-                      Like filing your GST returns, Auditing etc.
-                    </p>
-                    <p className="text-xl text-blue-500 text-center">
-                      Reach Out to Us.
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-center mt-4">
-                    <button
-                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-                      onClick={handleClick}
-                    >
-                      Start filing
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-      <footer className="bg-gray-800 text-white py-4">
+      </div>
+      <Routes>
+        <Route path="/form" element={<Form />} />
+        {/* Other routes */}
+      </Routes>
+    </main>
+    <footer className="bg-gray-800 text-white py-4">
         <div className="flex justify-between px-4">
           {/* Contact details */}
           <div className="flex flex-col">
@@ -275,8 +279,20 @@ const Home = () => {
           </div>
         </div>
       </footer>
+    {showForm && (
+  <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75">
+    <div className="bg-white p-8 rounded-lg">
+      <button className="absolute top-0 right-0 m-4 z-50" onClick={handleCloseForm}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
+      <Form onClose={handleCloseForm} />
     </div>
-  );
-};
+  </div>
+)}
+  </div>
+)
+}
 
 export default Home;
